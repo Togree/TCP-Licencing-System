33df6b552eaa3d60708be76028a82b2017be3349
Ahmed Sereta <seretaahmed18@gmail.com>
2025-03-22 22:14:14 +0300

    Added viewset for admin account creation

--- licenses/views.py
+++ licenses/views.py
@@ -1,3 +1,135 @@
-from django.shortcuts import render
+from django.contrib.auth.hashers import make_password
+from django.shortcuts import render, get_object_or_404
+from rest_framework import status, viewsets
+from rest_framework.exceptions import ValidationError
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from licenses.models import UserAccount
+
 
 # Create your views here.
+
+class UserViewSet(viewsets.ViewSet):
+    permission_classes_by_action = {'create': [AllowAny], 'list': [IsAdminUser], 'verify_otp': [AllowAny], 'default': [IsAuthenticated]}
+
+    def get_permissions(self):
+        return [permission() for permission in self.permission_classes_by_action.get(self.action, self.permission_classes_by_action['default'])]
+
+    def list(self, request):
+        try:
+            users = UserAccount.objects.all()
+            serializer = UserAccountSerializer(users, many=True, context={"request": request})
+            response_data = serializer.data
+            response_dict = {"error": False, "message": "All Users List Data", "data": response_data}
+
+        except ValidationError as e:
+            response_dict = {"error": True, "message": "Validation Error", "details": str(e)}
+        except Exception as e:
+            response_dict = {"error": True, "message": "An Error Occurred", "details": str(e)}
+
+        return Response(response_dict,
+                        status=status.HTTP_400_BAD_REQUEST if response_dict['error'] else status.HTTP_200_OK)
+
+    def create(self, request):
+        serializer = UserCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            # Hash the password before saving the user
+            password = make_password(serializer.validated_data['password'])
+            serializer.validated_data['password'] = password
+
+            # Set the user as inactive
+            serializer.validated_data['is_active'] = False
+            serializer.save()
+
+            return Response({'message': 'OTP sent to email'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        queryset = UserAccount.objects.all()
+        user = get_object_or_404(queryset, pk=pk)
+        serializer = UserAccountSerializer(user)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        user = UserAccount.objects.get(pk=pk)
+        serializer = UserAccountSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def partial_update(self, request, pk=None):
+        user = UserAccount.objects.get(pk=pk)
+        serializer = UserAccountSerializer(user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk=None):
+        user = UserAccount.objects.get(pk=pk)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class AdminUserViewSet(viewsets.ViewSet):
+    permission_classes_by_action = {
+        'create': [AllowAny],
+        'verify_otp': [AllowAny],
+        'default': [IsAuthenticated]
+    }
+
+    def get_permissions(self):
+        return [permission() for permission in self.permission_classes_by_action.get(self.action, self.permission_classes_by_action['default'])]
+
+    def create(self, request):
+        serializer = UserCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            # Hash the password before saving the user
+            password = make_password(serializer.validated_data['password'])
+            serializer.validated_data['password'] = password
+
+            # Set the user as active
+            serializer.validated_data['is_active'] = True
+            serializer.save()
+
+            return Response({'message': 'Account created succesfuly'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        queryset = UserAccount.objects.all()
+        user = get_object_or_404(queryset, pk=pk)
+        serializer = UserAccountSerializer(user)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        user = UserAccount.objects.get(pk=pk)
+        serializer = UserAccountSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def partial_update(self, request, pk=None):
+        user = UserAccount.objects.get(pk=pk)
+        serializer = UserAccountSerializer(user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk=None):
+        user = UserAccount.objects.get(pk=pk)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class UserInfoView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        serializer = CustomUserSerializer(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
